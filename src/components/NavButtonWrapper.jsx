e97abb245df1c69d04de25ea41cb0193d1417553
minjaeKim <smallkdb@gmail.com>
2022-08-04 19:00:22 +0900

    feat: add navbar buttons desgin

--- src/components/NavButtonWrapper.jsx
+++ src/components/NavButtonWrapper.jsx
@@ -2,18 +2,33 @@ import React from "react";
 import styled from "styled-components";
 
 const NavButton = styled.button`
-	color: blue;
+	color: black;
+	border: none;
+	background: none;
+	font-size: 16px;
+	width: 100%;
+	height: 100%;
+	:nth-child(2n-1) {
+		border-bottom: 2px solid #cfd2cf;
+		:hover {
+			border-bottom: 2px solid #f2f2f2;
+		}
+	}
+	:hover {
+		background: #f2f2f2;
+		transition: all 0.15s linear;
+	}
 `;
 
 const NavButtonWrapper = ({ categories, setCategory }) => {
 	return (
-		<div>
+		<>
 			{categories.map((c, index) => (
 				<NavButton key={index} onClick={() => setCategory(index)}>
 					{c}
 				</NavButton>
 			))}
-		</div>
+		</>
 	);
 };
 
